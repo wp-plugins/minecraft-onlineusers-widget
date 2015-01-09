4172d77bff8aad678ea8a3755bdb1ad1efaba2f1
pirmax <pirmax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 23:25:52 +0000

    Commit for 3.0 version of my plugin.
    git-svn-id: https://plugins.svn.wordpress.org/minecraft-onlineusers-widget/trunk@1064252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
-=== Minecraft Online Users Widget ===
+=== Minecraft Online Players Widget ===
 Contributors: pirmax
-Donate link: http://pirmax.fr/
-Tags: minecraft, online, users, widget, user, player, display, affichage, extension, plugin, afficher
-Requires at least: 3.0
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DPPT5J9GXXFJY
+Tags: minecraft, online, users, widget, user, player, players, display, affichage, extension, plugin, afficher
+Requires at least: 4.0
 Tested up to: 3.5.2
 Stable tag: trunk
 
@@ -10,7 +10,7 @@ Plugin Widget permettant d'afficher les joueurs en ligne d'un serveur Minecraft 
 
 == Description ==
 
-Le plugin <b>Minecraft OnlineUsers Widget</b> est un plugin permettant d'afficher les joueurs en ligne d'un serveur dans le menu du blog grâce à la fonction "query" de CraftBukkit.Soutenez le créateur de cette extension en vous abonnant à sa chaîne : <a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank">PirmaxLePoulpeRouge</a>.
+Le plugin **Minecraft Online Players Widget** est un plugin permettant d'afficher les joueurs en ligne d'un serveur Minecraft dans le menu du blog grâce à la fonction "query" de CraftBukkit.Soutenez le créateur de cette extension en vous abonnant à sa chaîne : <a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank">PirmaxLePoulpeRouge</a>.
 
 == Installation ==
 
@@ -18,13 +18,23 @@ L'installation est simple, mais vous devez cependant pouvoir modifier le contenu
 
 Ensuite, pour activer le widget, vous devez activer <code>enable-query</code> (<b>enable-query=true</b>) dans le fichier <code>server.properties</code> de votre serveur CraftBukkit puis red&eacute;marrer votre serveur.
 
-Rendez-vous dans le pannel des widgets sur votre blog Wordpress, et ajoutez le widget <code>Minecraft OnlineUsers</code>, configurez ensuite l'ip et le port du serveur ainsi que le nombre de slot de votre serveur, puis cliquez sur <code>Enregistrer</code>.
+Rendez-vous dans le pannel des widgets sur votre blog Wordpress, et ajoutez le widget <code>Minecraft Online Players</code>, configurez ensuite l'ip et le port du serveur ainsi que le nombre de slot de votre serveur, puis cliquez sur <code>Enregistrer</code>.
 
 == Screenshots ==
 
-1. Exemple d'affichage de deux joueurs en ligne
+1. Exemple d'affichage de plusieurs joueurs en ligne
+2. Exemple d'affichage de deux joueurs en ligne
 
 == Changelog ==
+
+= 3.0 =
+
+* Optimisation de certaines options
+* Mise à jour du Widget pour la version 4.0 de WordPress
+* Mise à jour du Widget dans le panel d'administration WordPress
+* Nettoyage du code CSS
+* Ajout de liens vers mes réseaux sociaux
+* Ajout d'un bouton de donation Paypal
 
 = 2.2 =
 
--- widget.php
+++ widget.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * @package Minecraft Online Users Widget
- * @version 2.2
+ * @package Minecraft Online Players Widget
+ * @version 3.0
  */
 /*
-Plugin Name: Minecraft Online Users Widget
+Plugin Name: Minecraft Online Players Widget
 Plugin URI: 
 Description: Plugin Widget permettant d'afficher les joueurs en ligne d'un serveur dans le menu du blog.
 Author: pirmax
-Version: 2.2
+Version: 3.0
 Author URI: http://pirmax.fr/
 */
 
-require_once(dirname(__FILE__) . '/MinecraftQuery.class.php');
+require_once(dirname(__FILE__) . '/lib/MinecraftQuery.class.php');
 
 function widget_mou()
 {
@@ -21,6 +21,44 @@ function widget_mou()
 }
 
 add_action('widgets_init', 'widget_mou');
+
+function widget_mou_link($links, $file)
+{
+    if($file == plugin_basename(__FILE__))
+	{
+        $widget_mou_link = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DPPT5J9GXXFJY" target="_blank">Faire un don</a>';
+        $links[] = $widget_mou_link;
+        $widget_mou_link = '<a href="https://wordpress.org/support/view/plugin-reviews/minecraft-onlineusers-widget" target="_blank">Voter pour ce plugin</a>';
+        $links[] = $widget_mou_link;
+    }
+    return $links;
+}
+
+add_filter('plugin_row_meta', 'widget_mou_link', 10, 2);
+
+function widget_mou_action_links($links, $file)
+{
+
+	static $this_plugin;
+
+	if(!$this_plugin)
+	{
+		$this_plugin = plugin_basename(__FILE__);
+	}
+
+	if($file == $this_plugin)
+	{
+		$settings_link = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DPPT5J9GXXFJY" target="_blank">Faire un don</a>';
+		array_unshift($links, $settings_link);
+		// $settings_link = '<a href="https://wordpress.org/support/view/plugin-reviews/minecraft-onlineusers-widget" target="_blank">Voter pour ce plugin</a>';
+		// array_unshift($links, $settings_link);
+	}
+
+	return $links;
+
+}
+
+add_filter('plugin_action_links', 'widget_mou_action_links', 10, 2 );
 
 class widget_mou extends WP_widget
 {
@@ -31,39 +69,24 @@ class widget_mou extends WP_widget
 			"description" => "Afficher les joueurs en ligne sur votre serveur Minecraft."
 		);
 
-		$this->WP_widget("widget-mou", "Minecraft Online Users", $options);
+		$this->WP_widget("widget-mou", "Minecraft Online Players", $options);
 	}
 
 	function widget($args, $instance)
 	{
 
+		$styleCSS = "#mouw {\n}\n\n#mouw li {\n\tfont-size: 15px;\n\tfont-weight: bold;\n}\n\n#mouw li img.avatar {\n\tvertical-align: middle;\n\tmargin-right: 10px;\n}\n\n.widget-title span.title {\n}\n\n.widget-title span.number {\n\tmargin-left: 20px;\n\tfont-weight: bold;\n\tfloat: right;\n}";
+
 		$defaut = array(
-			"title" => "Les joueurs en ligne",
+			"title" => "Joueurs en ligne",
 			"ifNoPlayer" => "Aucun joueur en ligne",
-			"serverip" => "play.minefight.fr",
+			"serverip" => "ip.mon-serveur.fr",
 			"serverport" => "25565",
 			"displayAvatar" => "on",
 			"displayCount" => "on",
 			"nbSlot" => 30,
 			"avatarSize" => 25,
-			"styleCSS" => "#mouw_li {
-	font-size: 15px;
-	font-weight: bold;
-}
-
-#mouw_li #mouw_avatar {
-	vertical-align: middle;
-	margin-right: 10px;
-}
-
-#mouw_title {
-	
-}
-
-#mouw_number {
-	margin-left: 20px;
-	font-weight: bold;
-}"
+			"styleCSS" => $styleCSS
 		);
 		$instance = wp_parse_args($instance, $defaut);
 
@@ -84,11 +107,10 @@ class widget_mou extends WP_widget
 				// echo $e->getMessage();
 			}
 
-			$displayWidget = '<style>' . $instance['styleCSS'] . '</style>';
-
 			extract($args);
 
-			$displayWidget .= '<ul id="mouw_ul">';
+			$displayWidget = '';
+			$displayWidget .= '<ul id="mouw">';
 
 			if($GetPlayers !== false)
 			{
@@ -96,28 +118,29 @@ class widget_mou extends WP_widget
 				{
 					if($instance['displayAvatar'] !== 1)
 					{
-						$displayWidget .= '<li id="mouw_li"><img src="https://minotar.net/helm/' . $value . '/' . $instance['avatarSize'] . '.png" width="' . $instance['avatarSize'] . '" height="' . $instance['avatarSize'] . '" border="0" title="' . $value . '" alt="avatar_' . $value . '" id="mouw_avatar" />' . $value . '</li>';
+						$displayWidget .= '<li><img src="https://minotar.net/helm/' . $value . '/' . $instance['avatarSize'] . '.png" width="' . $instance['avatarSize'] . '" height="' . $instance['avatarSize'] . '" border="0" title="' . $value . '" alt="avatar_' . $value . '" class="avatar" />' . $value . '</li>';
 					}
 					else
 					{
-						$displayWidget .= '<li id="mouw_li">' . $value . '</li>';
+						$displayWidget .= '<li>' . $value . '</li>';
 					}
 				}
 				$resnbPlayer = count($GetPlayers);
 			}
 			else
 			{
-				$displayWidget .= '<li id="mouw_li">' . $instance['ifNoPlayer'] . '</li>';
+				$displayWidget .= '<li>' . $instance['ifNoPlayer'] . '</li>';
 				$resnbPlayer = 0;
 			}
 
 			$displayWidget .= '</ul>';
 
+			echo '<style>' . $instance['styleCSS'] . '</style>';
 			echo $before_widget;
 
 			if($instance['displayCount'] !== 1)
 			{
-				echo $before_title . '<span id="mouw_title">' . $instance['title'] . '</span><span id="mouw_number">' . $resnbPlayer . '/' . $instance['nbSlot'] . '</span>' . $after_title;
+				echo $before_title . '<span class="title">' . $instance['title'] . '</span><span class="number">' . $resnbPlayer . '/' . $instance['nbSlot'] . '</span>' . $after_title;
 			}
 			else
 			{
@@ -139,74 +162,94 @@ class widget_mou extends WP_widget
 	function form($d)
 	{
 
+		$styleCSS = "#mouw {\n}\n\n#mouw li {\n\tfont-size: 15px;\n\tfont-weight: bold;\n}\n\n#mouw li img.avatar {\n\tvertical-align: middle;\n\tmargin-right: 10px;\n}\n\n.widget-title span.title {\n}\n\n.widget-title span.number {\n\tmargin-left: 20px;\n\tfont-weight: bold;\n\tfloat: right;\n}";
+
 		$defaut = array(
-			"title" => "Les joueurs en ligne",
+			"title" => "Joueurs en ligne",
 			"ifNoPlayer" => "Aucun joueur en ligne",
-			"serverip" => "play.minefight.fr",
+			"serverip" => "ip.mon-serveur.fr",
 			"serverport" => "25565",
 			"displayAvatar" => "on",
 			"displayCount" => "on",
 			"nbSlot" => 30,
 			"avatarSize" => 25,
-			"styleCSS" => "#mouw_li {
-	font-size: 15px;
-	font-weight: bold;
-}
-
-#mouw_li #mouw_avatar {
-	vertical-align: middle;
-	margin-right: 10px;
-}
-
-#mouw_title {
-	
-}
-
-#mouw_number {
-	margin-left: 20px;
-	font-weight: bold;
-}"
+			"styleCSS" => $styleCSS
 		);
 		$d = wp_parse_args($d, $defaut);
 
 		?>
 		<?php if(!function_exists('fwrite')){ echo '<p style="border-bottom: 1px dashed #FF0000; color: #FF0000; padding-bottom: 5px;"><b>Attention!</b> La fonction PHP <code>fwrite()</code> n\'est pas disponible sur votre hébergement. Contactez votre administrateur système.</p>'; } ?>
-		<p style="border-bottom: 1px dashed #CCCCCC; padding-bottom: 5px;">
-		Pour activer le widget, vous devez activer <code>enable-query</code> (<b>enable-query=true</b>) dans le fichier <code>server.properties</code> de votre serveur CraftBukkit puis red&eacute;marrer votre serveur.
+		<p>
+		Pour activer le widget, vous devez activer <code>enable-query</code> (<strong>enable-query=true</strong>) dans le fichier <code>server.properties</code> de votre serveur <strong>Minecraft</strong> puis red&eacute;marrer votre serveur.
 		</p>
+		<hr style="border-top: 1px dashed #CCCCCC;" />
 		<p>
 		<label for="<?php echo $this->get_field_id('title'); ?>">Titre du widget :</label><br />
-		<input value="<?php echo $d['title']; ?>" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" type="text" size="35" style="margin-left: 1em;" placeholder="Les joueurs en ligne" />
+		<input value="<?php echo $d['title']; ?>" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" type="text" class="widefat" placeholder="Les joueurs en ligne" />
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('ifNoPlayer'); ?>">Texte de remplacement :</label><br />
-		<input value="<?php echo $d['ifNoPlayer']; ?>" name="<?php echo $this->get_field_name('ifNoPlayer'); ?>" id="<?php echo $this->get_field_id('ifNoPlayer'); ?>" type="text" size="45" style="margin-left: 1em;" placeholder="Aucun joueur en ligne" />
+		<input value="<?php echo $d['ifNoPlayer']; ?>" name="<?php echo $this->get_field_name('ifNoPlayer'); ?>" id="<?php echo $this->get_field_id('ifNoPlayer'); ?>" type="text" class="widefat" placeholder="Aucun joueur en ligne" />
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('serverip'); ?>">Adresse IP du serveur :</label><br />
-		<input value="<?php echo $d['serverip']; ?>" name="<?php echo $this->get_field_name('serverip'); ?>" id="<?php echo $this->get_field_id('serverip'); ?>" type="text" size="35" style="margin-left: 1em;" placeholder="play.minefight.fr" /><br />
+		<input value="<?php echo $d['serverip']; ?>" name="<?php echo $this->get_field_name('serverip'); ?>" id="<?php echo $this->get_field_id('serverip'); ?>" type="text" class="widefat" placeholder="play.minefight.fr" /><br />
 		<label for="<?php echo $this->get_field_id('serverport'); ?>">Port du serveur :</label><br />
-		<input value="<?php echo $d['serverport']; ?>" name="<?php echo $this->get_field_name('serverport'); ?>" id="<?php echo $this->get_field_id('serverport'); ?>" type="text" size="20" style="margin-left: 1em;" placeholder="25565" />
+		<input value="<?php echo $d['serverport']; ?>" name="<?php echo $this->get_field_name('serverport'); ?>" id="<?php echo $this->get_field_id('serverport'); ?>" type="text" class="widefat" placeholder="25565" />
 		</p>
 		<p>
-		<label for="<?php echo $this->get_field_id('nbSlot'); ?>">Nombre de slot du serveur :</label><br />
-		<input value="<?php echo $d['nbSlot']; ?>" name="<?php echo $this->get_field_name('nbSlot'); ?>" id="<?php echo $this->get_field_id('nbSlot'); ?>" type="text" size="10" style="margin-left: 1em;" placeholder="30" /> slot(s)  <abbr title="Nombre de slot disponible sur votre serveur">(?)</abbr>
+		<label for="<?php echo $this->get_field_id('nbSlot'); ?>">Nombre de slot du serveur (<abbr title="Nombre de slot disponible sur votre serveur">?</abbr>) :</label><br />
+		<input value="<?php echo $d['nbSlot']; ?>" name="<?php echo $this->get_field_name('nbSlot'); ?>" id="<?php echo $this->get_field_id('nbSlot'); ?>" type="text" class="widefat" placeholder="30" />
 		</p>
 		<p>
-		<label for="<?php echo $this->get_field_id('avatarSize'); ?>">Taille des avatars :</label><br />
-		<input value="<?php echo $d['avatarSize']; ?>" name="<?php echo $this->get_field_name('avatarSize'); ?>" id="<?php echo $this->get_field_id('avatarSize'); ?>" type="text" size="10" style="margin-left: 1em;" placeholder="25" /> pixel(s)  <abbr title="Nombre de pixel (Longueur x Hauteur) de l'image">(?)</abbr>
+		<label for="<?php echo $this->get_field_id('avatarSize'); ?>">Taille des avatars (<abbr title="Nombre de pixel (Longueur x Hauteur) de l'image">?</abbr>) :</label><br />
+		<input value="<?php echo $d['avatarSize']; ?>" name="<?php echo $this->get_field_name('avatarSize'); ?>" id="<?php echo $this->get_field_id('avatarSize'); ?>" type="text" class="widefat" placeholder="25" />
 		</p>
 		<p id="editCSS">
-		<label for="<?php echo $this->get_field_id('styleCSS'); ?>">Modifier le style CSS :</label><br />
-		<textarea name="<?php echo $this->get_field_name('styleCSS'); ?>" id="<?php echo $this->get_field_id('styleCSS'); ?>" cols="36" rows="10"><?php echo $d['styleCSS']; ?></textarea>
+		<label for="<?php echo $this->get_field_id('styleCSS'); ?>">Modifier le style CSS (<a href="https://pastebin.com/u7H7G31e" target="_blank">CSS par défaut</a>) :</label><br />
+		<textarea name="<?php echo $this->get_field_name('styleCSS'); ?>" id="<?php echo $this->get_field_id('styleCSS'); ?>" class="widefat" rows="10" style="resize: vertical;"><?php echo $d['styleCSS']; ?></textarea>
 		</p>
 		<p>
 		<label for="<?php echo $this->get_field_id('displayAvatar'); ?>"><input name="<?php echo $this->get_field_name('displayAvatar'); ?>" id="<?php echo $this->get_field_id('displayAvatar'); ?>" type="checkbox" <?php if($d['displayAvatar'] == 'on'){ echo 'checked'; } ?> /> Afficher l'avatar des joueurs</label><br />
 		<label for="<?php echo $this->get_field_id('displayCount'); ?>"><input name="<?php echo $this->get_field_name('displayCount'); ?>" id="<?php echo $this->get_field_id('displayCount'); ?>" type="checkbox" <?php if($d['displayCount'] == 'on'){ echo 'checked'; } ?> /> Afficher le nombre de joueur en ligne</label>
 		</p>
-		<p style="border-bottom: 1px dashed #CCCCCC; padding-bottom: 5px;">
-		Soutenez le créateur de cette extension en vous abonnant à sa chaîne : <a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank">PirmaxLePoulpeRouge</a>.
+		<hr style="border-top: 1px dashed #CCCCCC;" />
+		<style>
+			.div-pirmax {
+				width: 100%;
+				text-align: center;
+			}
+			.div-pirmax ul.link-pirmax {
+				list-style: none;
+				width: 100%;
+			}
+			.div-pirmax ul.link-pirmax li {
+				margin: 0;
+				padding: 0px;
+				float: left;
+				display: inline;
+				width: 20%;
+				text-align: center;
+				font-size: 20px;
+			}
+			.div-pirmax ul.link-pirmax li a {
+				text-decoration: none;
+			}
+		</style>
+		<p align="center">
+			<div class="div-pirmax">
+				<ul class="link-pirmax">
+					<li><a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank" title="Twitter"><span class="dashicons dashicons-twitter"></span></a></li>
+					<li><a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank" title="YouTube"><span class="dashicons dashicons-video-alt3"></span></a></li>
+					<li><a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank" title="Facebook"><span class="dashicons dashicons-facebook"></span></a></li>
+					<li><a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank" title="Google+"><span class="dashicons dashicons-googleplus"></span></a></li>
+					<li><a href="http://www.youtube.com/user/PirmaxLePoulpeRouge" target="_blank" title="Blog"><span class="dashicons dashicons-wordpress"></span></a></li>
+				</ul>
+				<br><br>
+				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DPPT5J9GXXFJY" target="_blank"><img src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_SM.gif"></a>
+			</div>
 		</p>
+		<hr style="border-top: 1px dashed #CCCCCC;" />
 		<?php
 
 	}
